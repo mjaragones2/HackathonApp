9b338a67a04fc49fa145793137853d4556e37ef4
Mark Joshua Aragones <aragones.markjoshua25425@gmail.com>
2022-03-06 15:32:43 +0800

    Bootstrap

--- HackathonApp/App_Start/BundleConfig.cs
+++ HackathonApp/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace HackathonApp
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
